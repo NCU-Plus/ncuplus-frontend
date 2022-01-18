015cb120d4d177dab07f7cfd38c85fcfb701bc7b
ppodds <oscar20020629@gmail.com>
2022-01-18 14:56:39 +0800

    Feat: new mainpage of ncu+

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,20 @@
-<template>index</template>
+<template>
+  <fullpage-header>
+    <div class="absolute top-1/2 -translate-y-1/2 w-screen h-1/2">
+      <div class="h-full w-3/5 ml-auto mr-auto flex">
+        <div class="h-full w-1/2 font-sans">
+          <h1 class="text-7xl font-bold">NCU+</h1>
+          <h3 class="mt-5 text-2xl">中央大學智慧校園系統</h3>
+          <h4 class="mt-3 text-base">
+            A new hope at the place where Nobody Care U.
+          </h4>
+        </div>
+        <div class="h-full w-1/2"></div>
+      </div>
+    </div>
+  </fullpage-header>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FullpageHeader from "@/components/FullpageHeader.vue";
+</script>
