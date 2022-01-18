c104ecc4c8b0815dba4305613cafbc8aab417044
ppodds <oscar20020629@gmail.com>
2022-01-18 16:20:43 +0800

    Feat: about section

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <fullpage-header>
     <div class="absolute top-1/2 -translate-y-1/2 w-screen h-1/2">
-      <div class="h-full w-3/5 ml-auto mr-auto flex">
+      <div class="center-container w-3/5 flex">
         <div class="h-full w-1/2 font-sans">
           <h1 class="text-7xl font-bold">NCU+</h1>
           <h3 class="mt-5 text-2xl">中央大學智慧校園系統</h3>
@@ -13,6 +13,15 @@
       </div>
     </div>
   </fullpage-header>
+  <section class="h-screen w-screen"></section>
+  <section class="h-96 w-screen bg-slate-800 flex items-center">
+    <div class="h-4/5 w-full">
+      <div class="center-container w-3/5 text-white text-center">
+        <h1 class="text-2xl">About NCU+</h1>
+        <p></p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
