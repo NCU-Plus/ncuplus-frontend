6ff6645d9cdc63d715964bbb5ca5f0a7ebb62af7
ppodds <oscar20020629@gmail.com>
2022-01-19 21:44:35 +0800

    Feat: update course page

--- src/pages/courses/index.vue
+++ src/pages/courses/index.vue
@@ -1,1 +1,18 @@
-<template>courses</template>
+<template>
+  <div class="md:container mx-auto mt-12 mb-12 flex flex-col items-center">
+    <CourseSearch @search="onSearch" />
+    <table>
+      <tbody></tbody>
+      <tbody></tbody>
+    </table>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CourseSearch from "@/components/courses/CourseSearch.vue";
+import { SearchOptions } from "@/components/courses/SearchOptions";
+
+const onSearch = (options: SearchOptions) => {
+  // do search
+};
+</script>
