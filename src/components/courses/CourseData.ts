56a06ec00e2f385f5443f98d68dcd2ac0951dbaf
ppodds <oscar20020629@gmail.com>
2022-01-20 21:55:28 +0800

    Feat: add CourseData interface

---
+++ src/components/courses/CourseData.ts
@@ -0,0 +1,18 @@
+export interface CourseData {
+  year: number;
+  // 上 / 下
+  semester: string;
+  serialNo: number;
+  classNo: string;
+  title: string;
+  credit: number;
+  passwordCard: string;
+  teachers: string;
+  classTimes: string;
+  limitCnt: number;
+  admitCnt: number;
+  waitCnt: number;
+  collegeName: string;
+  departmentName: string;
+  courseType: string;
+}
