783a097310cbcbb723eda560d41790eb3e63eab5
ppodds <oscar20020629@gmail.com>
2022-01-21 22:54:30 +0800

    Feat: courses page course data

--- src/pages/courses/index.vue
+++ src/pages/courses/index.vue
@@ -10,23 +10,51 @@ import CourseSearch from "@/components/courses/CourseSearch.vue";
 import CourseList from "@/components/courses/CourseList.vue";
 import { SearchOptions } from "@/components/courses/SearchOptions";
 import { CourseData } from "@/components/courses/CourseData";
-import { reactive, ref, watch } from "vue";
+import { onBeforeMount, reactive, ref, watch } from "vue";
+import axios from "axios";
 
-// TODO: use axios get course data
-// import.meta.env.VITE_APP_API_URL
-const coursesData: CourseData[] = [];
+const coursesData = ref([] as CourseData[]);
+
+onBeforeMount(async () => {
+  const departmentMap = new Map<string, string>();
+  const collegeMap = new Map<string, string>();
+  const departments = axios.get(
+    import.meta.env.VITE_APP_API_URL + "/departments"
+  );
+  const colleges = axios.get(import.meta.env.VITE_APP_API_URL + "/colleges");
+  const courses = axios.get(import.meta.env.VITE_APP_API_URL + "/courses");
+  for (const department of (await departments).data.data)
+    departmentMap.set(department.departmentId, department.departmentName);
+  for (const college of (await colleges).data.data)
+    collegeMap.set(college.collegeId, college.collegeName);
+
+  for (const courseData of ((await courses).data.data as any[]).map(
+    (course) => {
+      return {
+        ...course,
+        collegeName: collegeMap.get(course.collegeId),
+        departmentName: departmentMap.get(course.departmentId),
+        classTimes: (JSON.parse(course.classTimes) as string[]).join(", "),
+        teachers: (JSON.parse(course.teachers) as string[]).join(", "),
+      };
+    }
+  )) {
+    coursesData.value.push(courseData);
+    filteredCoursesData.value.push(courseData);
+  }
+});
 
 const searchOptions = reactive({
   semester: "",
   department: "",
 } as SearchOptions);
 
-let filteredCoursesData = ref(coursesData);
+const filteredCoursesData = ref([] as CourseData[]);
 
 watch(
   () => ({ ...searchOptions }),
   () => {
-    filteredCoursesData.value = coursesData
+    filteredCoursesData.value = coursesData.value
       .filter((courseData) => {
         if (searchOptions.semester !== "")
           return (
