0ae539b4177c2cc2e800fccb1a160099de12201d
ppodds <oscar20020629@gmail.com>
2022-01-22 14:36:23 +0800

    Fix: main page miss other section after change route

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,31 +1,33 @@
 <template>
-  <fullpage-header class="bg-black">
-    <animation-background
-      :backgroundImages="backgroundImages"
-      :duration="{ enter: 1200, leave: 2000, stay: 8000, transfer: 200 }"
-    />
-    <div class="absolute top-1/2 -translate-y-1/2 w-screen h-1/2 text-white">
-      <div class="center-container w-3/5 flex">
-        <div class="h-full w-1/2 font-sans">
-          <h1 class="text-7xl font-bold">NCU+</h1>
-          <h3 class="mt-5 text-2xl">中央大學智慧校園系統</h3>
-          <h4 class="mt-3 text-base">
-            A new hope at the place where Nobody Care U.
-          </h4>
+  <div>
+    <fullpage-header class="bg-black">
+      <animation-background
+        :backgroundImages="backgroundImages"
+        :duration="{ enter: 1200, leave: 2000, stay: 8000, transfer: 200 }"
+      />
+      <div class="absolute top-1/2 -translate-y-1/2 w-screen h-1/2 text-white">
+        <div class="center-container w-3/5 flex">
+          <div class="h-full w-1/2 font-sans">
+            <h1 class="text-7xl font-bold">NCU+</h1>
+            <h3 class="mt-5 text-2xl">中央大學智慧校園系統</h3>
+            <h4 class="mt-3 text-base">
+              A new hope at the place where Nobody Care U.
+            </h4>
+          </div>
+          <div class="h-full w-1/2"></div>
         </div>
-        <div class="h-full w-1/2"></div>
       </div>
-    </div>
-  </fullpage-header>
-  <section class="h-screen w-full bg-white"></section>
-  <section class="h-96 w-full bg-slate-800 flex items-center">
-    <div class="h-4/5 w-full">
-      <div class="center-container w-3/5 text-white text-center">
-        <h1 class="text-2xl">About NCU+</h1>
-        <p></p>
+    </fullpage-header>
+    <section class="h-screen w-full bg-white"></section>
+    <section class="h-96 w-full bg-slate-800 flex items-center">
+      <div class="h-4/5 w-full">
+        <div class="center-container w-3/5 text-white text-center">
+          <h1 class="text-2xl">About NCU+</h1>
+          <p></p>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
